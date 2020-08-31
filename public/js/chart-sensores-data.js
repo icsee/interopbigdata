3dd38b0aa550715d3e0394e0f41049880fd0aedb
juanamezquita <t.amezquita.juancarlos@gmail.com>
2020-08-31 15:24:38 -0500

    Se actualiza la vista de sensores

--- public/js/chart-sensores-data.js
+++ public/js/chart-sensores-data.js
@@ -9,7 +9,74 @@ $(document).ready(() => {
 
   let paquetes = 0
   let start = new Date()
-
+  var optionsAnimations = {
+    animation: false,
+	maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 25,
+        bottom: 0
+      }
+    },  
+    legend: {
+      display: false
+    },
+    responsive: true,
+	scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          maxTicksLimit: 5,
+          padding: 10,
+          // Include a dollar sign in the ticks
+          callback: function(value, index, values) {
+            return number_format(value);
+          }
+        },
+        gridLines: {
+          color: "rgb(234, 236, 244)",
+          zeroLineColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        }
+      }],
+    },
+	tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 14,
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      intersect: false,
+      mode: 'index',
+      caretPadding: 10,
+      callbacks: {
+        label: function(tooltipItem, chart) {
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+          return datasetLabel + number_format(tooltipItem.yLabel);
+        }
+      }
+    }
+  }
   /*
 
 
@@ -69,7 +136,7 @@ borderColor: '#F44436',
           data2.datasets[0].data.shift()
           data2.labels.shift()  
         }
-
+        
         data2.labels.push(moment().format('HH:mm:ss'))
         data2.datasets[0].data.push(messageData.IotData.Lora)
       
@@ -99,8 +166,8 @@ borderColor: '#F44436',
     
     //chart1.update()
     chart2.update()
-    //chart3.update()
-   // chart4.update()
+
+
    
      
       
